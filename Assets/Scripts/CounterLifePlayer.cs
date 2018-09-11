7b78af64fd197134aaa694a7317315e39ef4640f
Andrew Yang <aQy8je@virginia.edu>
2018-09-11 12:55:59 -0700

    Fixed playter counter but and created prefab upon bugfix testing.

--- Assets/Scripts/CounterLifePlayer.cs
+++ Assets/Scripts/CounterLifePlayer.cs
@@ -46,7 +46,8 @@ public class CounterLifePlayer : MonoBehaviour {
         {
             if (currentLifeIconIndex < 0)
             {
-                // log error
+                // log error?
+                break;
             }
             Destroy(healthIcons[currentLifeIconIndex]);
             currentLifeIconIndex--;
